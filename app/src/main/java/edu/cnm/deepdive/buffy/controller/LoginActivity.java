f3fc9d30ed1176c986413d96ea1c1131d6f3ac5d
Jason Shaner <jshaner@cnm.edu>
2020-07-15 15:15:25 -0600

    Added Google Sign In Button.

---
+++ app/src/main/java/edu/cnm/deepdive/buffy/controller/LoginActivity.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive.buffy.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import edu.cnm.deepdive.buffy.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+  }
+}
